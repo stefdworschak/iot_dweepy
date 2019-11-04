2d447c16a024355a1b1206ab647d5b6e63017258
Stefan Dworschak <stef.dworschak@gmail.com>
2019-11-04 22:31:51 +0000

    Added light sensor measurement

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ led = 5
 # Analog input ports
 # SIG,NC,VCC,GND
 potentiometer = 0
+light_sensor = 1
 #emperatur_sensor = 1
 
 dht_sensor_port = 7
@@ -30,11 +31,14 @@ grove_vcc = 5
 # Full value of the rotary angle is 300 degrees, as per it's specs (0 to 300)
 full_angle = 300
 
+grovepi.pinMode(light_sensor,"INPUT")
+
 while True:
     try:
         temp = {}
         # Retrieve the potentiometer value
         potentiometer_value = grovepi.analogRead(potentiometer)
+        light_sensor_value = grovepi.analogRead(light_sensor)
         #temperature_value = grovepi.temp(temperatur_sensor,'1.1')
         [ tempr,hum ] = dht(dht_sensor_port,dht_sensor_type)
 
@@ -49,7 +53,8 @@ while True:
         # Fill dict with all readings
         temp['voltage'] = voltage
         temp['degrees'] = degrees
-        temp['brightness'] = brightness
+        temp['illuminance'] = light_sensor_value
+        #temp['brightness'] = brightness
         temp['temperature'] = tempr
         temp['humidity'] = hum
 
