643c6217fa27e3d5b2412a9fa2c30749da9456d8
Stefan Dworschak <stef.dworschak@gmail.com>
2019-11-05 00:09:21 +0000

    Adding to insert record to mongodb collection

--- mongo_db.py
+++ mongo_db.py
@@ -3,6 +3,6 @@ import pymongo
 def insert_into(record):
     myclient = pymongo.MongoClient('mongodb://localhost:28017/')
     db = myclient['iot_dweepy']
-    #col = db['records']
+    col = db['records']
 
     #x = col.insert_one(record)
