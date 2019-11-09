d7bea3468ca0c034b7f0c11f47e8deef1a3757ac
Stefan Dworschak <stef.dworschak@gmail.com>
2019-11-09 09:56:18 +0000

    Add new sensors and lcd screen

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import time
 import json
+import datetime
 
 import grovepi
 from grovepi import *
@@ -10,17 +11,20 @@ import mongo_db as m
 
 # Digital input ports
 # SIG,NC,VCC,GND
-led = 5
+#led = 5 # LED on D5
+dht_sensor_port = 7 # Temperature and Humidity sensor on D7
 
 # Analog input ports
 # SIG,NC,VCC,GND
-potentiometer = 0 # Angle sensor
-light_sensor = 1
-sound_sensor = 2
+potentiometer = 0 # Angle sensor on A0
+light_sensor = 1 # Light sensor on A1
+#sound_sensor = 2 # Sound sensor on A2
 #emperatur_sensor = 1
+button_sensor = 4
 
-dht_sensor_port = 7 # Temperature and Humidity sensor
+# Setting the sensor type for the Temperature and Humidity Sensor
 dht_sensor_type = 0
+
 
 ###################
 #### Constants ####
@@ -39,34 +43,38 @@ full_angle = 300
 sound_threshold = 400
 
 grovepi.pinMode(light_sensor,"INPUT")
-grovepi.pinMode(sound_sensor,"INPUT")
+grovepi.pinMode(button_sensor,"INPUT")
+setRGB(0,255,0)
 
 last = {}
 
 while True:
     try:
         temp = {}
+
         # Retrieve the potentiometer value
         potentiometer_value = grovepi.analogRead(potentiometer)
         light_sensor_value = grovepi.analogRead(light_sensor)
-        sound_sensor_value = grovepi.analogRead(sound_sensor)
-        #temperature_value = grovepi.temp(temperatur_sensor,'1.1')
         [ tempr,hum ] = dht(dht_sensor_port,dht_sensor_type)
+
+        #Set Time on LCD
+        setText_norefresh(datetime.datetime.now().isoformat())
 
         # Retrieve secondary values based on potentiometer
         voltage = round((float)(potentiometer_value) * adc_ref / 1023, 2)
         degrees = round((voltage * full_angle) / grove_vcc, 2)
-        brightness = int(degrees / full_angle * 255)
+        #brightness = int(degrees / full_angle * 255)
 
         # Set the brightness for the LED
-        grovepi.analogWrite(led,brightness)
+        #grovepi.analogWrite(led,brightness)
         
         # Fill dict with all readings
         temp['voltage'] = voltage
         temp['degrees'] = degrees
         temp['illuminance'] = light_sensor_value
-        temp['sound_value'] = sound_sensor_value
-        temp['sound_category'] = 'loud' if sound_sensor_value > sound_threshold else 'silent'
+        temp['button_value'] = grovepi.digitalRead(button_sensor)
+        #temp['sound_value'] = sound_sensor_value
+        #temp['sound_category'] = 'loud' if sound_sensor_value > sound_threshold else 'silent'
         #temp['brightness'] = brightness
         temp['temperature'] = tempr
         temp['humidity'] = hum
@@ -89,7 +97,7 @@ while True:
         time.sleep(1)
 
     except KeyboardInterrupt:
-        grovepi.analogWrite(led,0)
+        #grovepi.analogWrite(led,0)
         break
     except IOError:
         break
