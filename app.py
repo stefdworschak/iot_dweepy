fef6a405184f95219ba2652cddfceb6f32c27dac
Stefan Dworschak <stef.dworschak@gmail.com>
2019-11-15 22:38:20 +0000

    changing speed of count to 1

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ while True:
 
         # Set a timeout of one second
         last = temp
-        time.sleep(0.7)
+        time.sleep(0.6)
 
     except (IOError, TypeError, NameError) as e:
         print(str(e))
