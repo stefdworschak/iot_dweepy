410caae77d5cf4821d386bb06bbac1413f04e9ee
Stefan Dworschak <stef.dworschak@gmail.com>
2019-11-17 18:15:40 +0000

    General cleanup, removing test files and unused files

--- run.py
+++
@@ -1,8 +0,0 @@
-import dweepy 
-import inspect
-
-dweepy.dweet_for('sd_dwo_20191102',{'message':'Hello'})
-dweepy
-#dweeps = dweepy.get_dweets_for('sd_dwo_20191102')
-#print(dweeps)
-#print(inspect.getmembers(dweepy, predicate=inspect.ismethod))
--- test.py
+++
@@ -1,8 +0,0 @@
-import json
-
-temp = {}
-
-with open('data.json') as file:
-    json_data = json.loads(file.read())
-    thing_id = json_data['thing_id']
-    temp['location'] = json_data['location']
